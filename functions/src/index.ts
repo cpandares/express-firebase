bda9473362c5f8943f945d272e587370d11d56ba
unknown <pandaresocesar@gmail.com>
2020-08-05 15:34:35 -0400

    email send ok

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
 
+
 import * as admin from 'firebase-admin';
 
 const serviceAccount = require('./serviceAccountKey.json');
@@ -14,9 +15,7 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+
  export const helloWorld = functions.https.onRequest((request, response) => {
    functions.logger.info("Hello logs!", {structuredData: true});
    response.json("Hola desde mi nuevo proyecto");
@@ -35,13 +34,22 @@ const db = admin.firestore();
         res.json( works );
  });
 
-        
+export const getLevel = functions.https.onRequest( async(req, res)=>{
+    const levelRef = db.collection('skills');
+    const levelSnap = await levelRef.get();
+    const level = levelSnap.docs.map(doc=>doc.data);
+
+    res.json(level);
+});        
 
 
  const app = express();
-
+ const nodemailer = require("nodemailer");
  app.use( cors({origin : true}) );
 
+ app.use(express.urlencoded({ extended:false }));
+
+ app.use(express.json());
  
  app.get('/works', async(req, res)=>{
 
@@ -69,9 +77,53 @@ const db = admin.firestore();
             });
             console.log("Error");
         }
-        
-        
-
+   
  });
+ app.get('/levels', async(req, res)=>{
+    const levelRef = db.collection('skills');
+    const levelSnap = await levelRef.get();
+    const level = levelSnap.docs.map(doc=>doc.data());
+
+    res.json(level);
+});
+
+app.post('/send-email', async(req, res)=>{
+    const {name, email, phone, message} = req.body;
+
+    const contentHTML =  ` 
+            <h1>Información de Contacto</h1>
+            <ul>
+                <li>Nombre: ${name}</li>
+                <li>Email: ${email}</li>
+                <li>Telefono: ${phone}</li>
+               
+            </ul>
+            <br>
+            <p>Mensaje: ${message}</p>
+    ` ;
+
+    const transporter = nodemailer.createTransport({
+        host: "mail.beconection.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: 'testmail@beconection.com', // generated ethereal user
+          pass: 'contraseña' // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+      });
+
+   const info = await transporter.sendMail({
+          from: "'Calitriqui Server' <testmail@beconection.com>",
+          to:'pandaresocesar@gmail.com',
+          subject:"Formulario de Portafolio",
+          html: contentHTML
+      });
+      console.log("Mensaje Enviado", info.messageId);
+    res.json('recibido');
+});
+
 
  export const api = functions.https.onRequest( app );
